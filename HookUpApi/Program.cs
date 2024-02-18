a9f028a61fc912c7daddec917db1eadefd28ddde
Sandeep Murmu <murmusandeep17@gmail.com>
2024-02-18 12:28:59 +0530

    chapter 2 completed

--- HookUpApi/Program.cs
+++ HookUpApi/Program.cs
@@ -12,9 +12,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors("CorsPolicy");
 
 app.MapControllers();
 
