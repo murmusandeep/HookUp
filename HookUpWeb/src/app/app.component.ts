c2e9cf8465f7dce0085cb82305be268935697fe6
Sandeep Murmu <murmusandeep17@gmail.com>
2024-02-22 22:34:45 +0530

    chatper 4 completed

--- HookUpWeb/src/app/app.component.ts
+++ HookUpWeb/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { User } from './_model/user';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +10,17 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   title = 'HookUpWeb';
-  users: any;
-  constructor(private http: HttpClient) { }
+
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/Users').subscribe({
-      next: (response) => {
-        this.users = response;
-        console.log(response);
-      },
-      error: () => { },
-      complete: () => console.log("Request is completed")
-    });
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 }
